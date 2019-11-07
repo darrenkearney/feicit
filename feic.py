8fac03b5eaadd1535e9d8de8f837a78565334753
Darren Kearney <darren@darrenk.net>
2019-11-07 17:53:07 +0000

    Modified strings, added license header info to source files.
    	modified:   feic.py

--- feic.py
+++ feic.py
@@ -1,4 +1,20 @@
 #!/usr/env/python3
+"""'feicit' is a minimal terminal-based web browser, to view websites as plain text.
+Copyright (C) 2019 Darren Kearney
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import html
 import html2text
@@ -35,7 +51,24 @@ tl;dr - stop now and quit with Ctrl+C unless you understand the risks and wish t
 You have been warned.
 ''',
     'oktnxbye': '''Buíochas as féachaint ar an Idirlíon le "feckit", slán!
-Thanks for feicing the internet, bye!'''
+Thanks for feicing the internet, bye!''',
+    'gpl3': '''feicit  Copyright (C) 2019  Darren Kearney
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.''',
+    'gpl3_warranty': '''
+      THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+      APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+      HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
+      OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
+      THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+      PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
+      IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
+      ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+    ''',
+    'gpl3_conditions':''' The conditions have yet to be added to interactive terminal in this software. Please see the license document that came with this software for more about the conditions.
+    '''
+
 }
 
 def get_address_from_input():
@@ -47,11 +80,20 @@ def get_address_from_input():
         print(messages['warning'])
         print('Enter "quit" to quit.')
         address = input("URL > ")
+        print(address)
 
         if (address == 'quit' or address == 'exit' or address == '"quit"'):
             print(messages['oktnxbye'])
             exit()
 
+        elif (address == 'show c'):
+            print(messages['gpl3_conditions'])
+            exit()
+
+        elif (address == 'show w'):
+            print(messages['gpl3_warranty'])
+            exit()
+        
         # add missing schema
         if (address[:8] != 'https://' and address[:7] != 'http://'):
             # default to https
@@ -81,6 +123,7 @@ def main():
 
     try:
         while True:
+
             print(parse_content_to_md(get_contents(get_address_from_input())))
 
 
